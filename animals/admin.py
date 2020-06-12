c65effce6a7491e90af2b41942180dbef10354de
luisfff29 <luisfff29@gmail.com>
2020-06-12 10:52:23 -0500

    update admin page

--- animals/admin.py
+++ animals/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
+from animals.models import Animals
+
 
 # Register your models here.
+admin.site.register(Animals, DraggableMPTTAdmin)
