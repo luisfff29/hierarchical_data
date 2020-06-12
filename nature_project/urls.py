25515f3c5e67425a95316424031b4286d5723154
luisfff29 <luisfff29@gmail.com>
2020-06-12 13:26:20 -0500

    add url to create form

--- nature_project/urls.py
+++ nature_project/urls.py
@@ -21,5 +21,5 @@ from animals import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('create/', views.Create_Animal.as_view()),
+    path('create/', views.Create_Animal.as_view(), name='create'),
 ]
