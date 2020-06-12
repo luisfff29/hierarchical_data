c09d523d70e4606819d35ff54a13f3e8ebd25d55
luisfff29 <luisfff29@gmail.com>
2020-06-12 10:54:20 -0500

    create view page

--- animals/views.py
+++ animals/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from animals.models import Animals
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {'animals': Animals.objects.all()})
